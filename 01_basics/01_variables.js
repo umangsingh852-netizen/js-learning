4ce07eb8e3b1d5b0ad01ce506d8a477fd6fe93d7
Uditanshu Singh <umangsingh852@gmail.com>
2026-08-27 21:03:32 +0000

    conversions and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail = "udit@gmail.com"
+var accountPassword = "Roommate"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 123
+accountEmail = "udit@gmmmm.com"
+accountPassword = "Rootmate"
+accountCity = "hapur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strics"; //treat all JS code a newer version
+
+// alert(3 + 3) //we are using node js not browser
+
+console.log(3 + 3)
+console.log("Udit")
+
+let name = "Udit"
+let age = 21
+let isLoggedin = false
+
+// number => 2 to power 53
+// bigint =>
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undeifned => no value given yet
+ // symbol => unique
+// object 
+
+console.log(typeof age);
