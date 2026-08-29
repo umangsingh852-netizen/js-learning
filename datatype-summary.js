4d5504c179b28427e80a39d9722ca21abd2f52cf
Uditanshu Singh <umangsingh852@gmail.com>
2026-08-29 09:31:30 +0000

    datatype summary

---
+++ datatype-summary.js
@@ -0,0 +1,37 @@
+// primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedin = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId)
+
+const bigNumber = 123456543234554n
+
+
+// Reference (Non Primitive)
+
+// Arrays, Object, Functions
+
+//Arrays example 
+
+const language = ["python", "java", "sql"]
+let myObj = {
+    name: "Udit",
+    age: 21
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof anotherId);
+
