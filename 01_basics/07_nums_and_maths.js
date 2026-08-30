4af7bd21dd50d1e494a0cde038f5fdf69ef35d52
Uditanshu Singh <umangsingh852@gmail.com>
2026-08-30 08:36:15 +0000

    math and some string applications like ceil floor etc also max min random number

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,34 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.tofixed(1));
+
+const otherNumber = 123.935
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//******************MATHS********************** */
+
+// console.log(Math); 
+// console.log(Math.abs(4));
+// console.log(Math.round(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.ceil(4.3));
+
+// console.log(Math.random());
+console.log(Math.ceil(Math.random()*100) + 1);
+console.log(Math.floor(Math.random()*100) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1)) + min)
+
