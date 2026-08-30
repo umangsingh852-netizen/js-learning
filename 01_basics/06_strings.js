c6005071ddd82d89922bdb8962b90b9bdbc6f581
Uditanshu Singh <umangsingh852@gmail.com>
2026-08-30 06:26:12 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,18 @@
+const name = "Jofferey"
+const repoCount = 50
+
+console.log("");
+
+const gameName = new String('Jofferey')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+
+const url = "https://hitehs.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+console.log(url.includes('Jofferey'))
