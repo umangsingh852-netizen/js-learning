571a16cf1617ceb3f243de60c59073c2288822bc
Uditanshu Singh <umangsingh852@gmail.com>
2026-09-02 07:35:42 +0000

    more in objects adn some prototypes

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -17,4 +17,38 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email:"udit1@Mail.com"
+    },
+    {
+        id: 2,
+        email:"udit2@Mail.com"
+    },
+    {
+        id: 3,
+        email:"udit3@Mail.com"
+    }
+]
+
+// console.log(users[1].email)
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('IsLoggedIn'));
