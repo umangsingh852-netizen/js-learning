4997dee0c5ecfb21ba45b24078ad414ccc9220e5
Uditanshu Singh <umangsingh852@gmail.com>
2026-09-04 12:17:04 +0000

    iife function

--- 03_Baiscs/04_iife.js
+++ 03_Baiscs/04_iife.js
@@ -8,7 +8,7 @@
 //Never forget the semicolon when calling two iife function
 
 ((name) => {
-    console.log(`BD CONNECTED ${name}`):
+    console.log(`BD CONNECTED ${name}`);
 })('Udit')
 // Global scope ke pollution se problrm hoti h keyi baar to us pollution ko htane ke liye humne iife ka use kiya
 
