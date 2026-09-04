f75ed192066d01eb36fa2c34f2b0ee261f28d761
Uditanshu Singh <umangsingh852@gmail.com>
2026-09-04 08:28:30 +0000

    iife functions

--- 03_Baiscs/03_arrow.js
+++ 03_Baiscs/03_arrow.js
@@ -29,3 +29,4 @@ const addTwo = (num1, num2) => (num1 + num2)
 
 console.log(addTwo(3, 4))
 
+ 
---
+++ 03_Baiscs/04_iife.js
@@ -0,0 +1,14 @@
+// IIFE
+
+(function chai(){
+    console.log(`DB Managed`);
+})();
+
+
+//Never forget the semicolon when calling two iife function
+
+((name) => {
+    console.log(`BD CONNECTED ${name}`):
+})('Udit')
+// Global scope ke pollution se problrm hoti h keyi baar to us pollution ko htane ke liye humne iife ka use kiya
+
