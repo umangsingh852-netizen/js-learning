ed3b67f761a39275ca01d5d790c8eae991849c48
Uditanshu Singh <umangsingh852@gmail.com>
2026-09-04 08:12:19 +0000

    this and arrow function

---
+++ 03_Baiscs/03_arrow.js
@@ -0,0 +1,31 @@
+const user = {
+    username: "Udit",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username}, welcome to website`);
+    }
+}
+// user.welcomeMessage()
+user.username = "Sam"
+// user.welcomeMessage()
+
+// function chai(){
+//     let username = "Udit"
+//     console.log(this.username)
+// }
+
+// chai()
+
+const chai = () => {
+    let username = "Udit"
+    console.log(this);
+
+}
+chai()
+
+const addTwo = (num1, num2) => num1 + num2
+const addTwo = (num1, num2) => (num1 + num2)
+
+console.log(addTwo(3, 4))
+
